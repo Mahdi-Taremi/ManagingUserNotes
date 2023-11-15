6aad109ebc3457fbe1ab0abdb1d205e56dfb7a74
MahdiTaremi <taremiam@gmail.com>
2023-11-15 23:23:55 +0330

    Define tests with scenarios for User

---
+++ ManagingUserNotes.Test/Controller/UserControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagingUserNotes.Test.Controller
+{
+    public class UserControllerTests
+    {
+        public UserControllerTests()
+        {
+            
+        }
+
+        // Return a json contains all users without notes
+        [Fact]
+        public void CanGetAllUsers_GetUsers_ReturnAJsonContainsAllUsersWithoutNotes()
+        {
+            // Arrange 
+
+            // Act 
+
+            // Assert 
+
+        }
+
+        // Return a json contains user with all notes
+        [Fact]
+        public void CanGetOneUserById_GetUserById_ReturnAJsonContainsUserWithAllNotes()
+        {
+            // Arrange 
+
+            // Act 
+
+            // Assert 
+
+        }
+    }
+}
