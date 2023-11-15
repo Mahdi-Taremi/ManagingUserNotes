bc3a25c73e25a4ae666bc8d71a6c7abf7a01c891
MahdiTaremi <taremiam@gmail.com>
2023-11-15 23:55:44 +0330

    Define tests with scenarios for Note

---
+++ ManagingUserNotes.Test/Controller/NoteControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagingUserNotes.Test.Controller
+{
+    public class NoteControllerTests
+    {
+        public NoteControllerTests()
+        {
+            
+        }
+
+        // Return a json contains all notes for a user
+        [Fact]
+        public void CanGetNotesFromAUser_GetNotesByUserId_ReturnAJsonContainsAllNotesForAUser()
+        {
+            // Arrange 
+
+            // Act 
+
+            // Assert 
+
+        }
+
+        // Return a json contains note
+        [Fact]
+        public void CanGetNoteWithId_GetNoteById_ReturnAJsonContainsNote()
+        {
+            // Arrange 
+
+            // Act 
+
+            // Assert 
+
+        }
+    }
+}
