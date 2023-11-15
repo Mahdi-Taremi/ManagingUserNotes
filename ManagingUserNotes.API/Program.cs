8f127a2a5b1e62a16ae3ced94b030e7ccbce5f36
MahdiTaremi <taremiam@gmail.com>
2023-11-15 22:30:51 +0330

    Add Swagger and Manage Routing

---
+++ ManagingUserNotes.API/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+/*  *** Add services to the container. *** */
+#region services
+// use Controllers
+builder.Services.AddControllers();
+// 1.(Swagger) First stage for add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+#endregion
+
+var app = builder.Build();
+
+/* *** Configure the HTTP request pipeline. *** */
+#region Pipeline
+// 2.(Swagger) Second  stage for add Swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Manage Routing
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+#endregion
+
+
+
+app.Run();
