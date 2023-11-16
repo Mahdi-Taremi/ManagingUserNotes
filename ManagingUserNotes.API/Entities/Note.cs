4c68bcb511c218f5f1457742d4fe9c07000ed261
MahdiTaremi <taremiam@gmail.com>
2023-11-16 12:04:45 +0330

    Add Entities for user and note

---
+++ ManagingUserNotes.API/Entities/Note.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManagingUserNotes.API.Entities
+{
+    public class Note
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        // DataType(DataType.Date)] Validation N/A
+        public DateTime? DateCreated { get; set; }
+
+        [Required]
+        // DataType(DataType.Date)] Validation N/A
+        public DateTime? DateModified { get; set; } = DateTime.Now;
+
+        [Required]
+        public int Views { get; set; }
+
+        [Required]
+        public bool Published { get; set; }
+
+        // Relation with user --> 1
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ ManagingUserNotes.API/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManagingUserNotes.API.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(128)]
+        public string FirstName { get; set; }
+        [StringLength(128)]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "The Email field is required.")]
+        [EmailAddress(ErrorMessage = "The Email field is not a valid e-mail address.")]
+        public string Email { get; set; }
+        [Required]
+        [Range(0, 160, ErrorMessage = "The Age should be between 0 and 160 years.")]
+        public int Age { get; set; }
+        // [Url] Validation N/A
+        public string? Website { get; set; }
+
+        // Relation with note --> N
+        public ICollection<Note> Note { get; set; } = new List<Note>();
+    }
+}
